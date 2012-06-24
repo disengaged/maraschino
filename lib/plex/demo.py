696f079829ee53ea6b677a5aa7e390d237f43743
Anne Jan Elsinga <anne.jan@ajelsinga.nl>
2012-06-24 11:49:02 +0200

    Added small demo program to plexclient library

---
+++ lib/plex/demo.py
@@ -0,0 +1,26 @@
+from plexclient import PLEXClient, PLEXLibrary
+'''
+@author: Anne Jan Elsinga
+This scripts serves as a demo for what the plex client library can do.
+'''
+
+#===============================================================================
+#  ask for Plex Media Server address and port
+#===============================================================================
+PlexServer=raw_input("Enter Plex Media Server:     ")
+PlexPort=raw_input("Enter Plex Media Server port:")
+
+MessageTitle="Test message"
+MessageText="Sent to all clients"
+
+server=PLEXLibrary(PlexServer,PlexPort)
+for servert in server.getclients():
+    print servert.address
+    print servert.uniqueid
+    print servert.version
+    print servert.name
+    print servert.host
+    client=PLEXClient(servert.host,servert.port)
+    client.sendmessage(MessageTitle+","+MessageText)
+    
+
