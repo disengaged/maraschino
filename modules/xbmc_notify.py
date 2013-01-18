8eca7cb00eb1a9ef6ab978957c80d9f0186067fd
Richard Evans <bob3695@gmail.com>
2013-01-18 07:22:08 -0500

    xbmc_notify now works with XBMC and Plex!

--- modules/xbmc_notify.py
+++ modules/xbmc_notify.py
@@ -6,6 +6,8 @@ from socket import *
 from xbmc.xbmcclient import *
 from maraschino.tools import get_file_list
 from maraschino.models import XbmcServer
+from maraschino.noneditable import *
+from plex.plexclient import PLEXLibrary, PLEXClient
 
 @app.route('/xhr/xbmc_notify', methods=['post'])
 def xhr_notify():
@@ -31,35 +33,51 @@ def xhr_notify_message():
     hostname = str(request.form['hostname'])
     message = str(request.form['message'])
     title = str(request.form['title'])
-    port = 9777
-    icon = os.path.join(RUNDIR, 'static', 'images', 'notifications', request.form['image'])
 
     if title == "Title":
         title = "Maraschino"
 
-    if not os.path.exists(icon):
-        icon = os.path.join(RUNDIR, 'static', 'images', 'maraschino_logo.png')
+    if (server_type() == "XBMC"):
+        port = 9777
+        icon = os.path.join(RUNDIR, 'static', 'images', 'notifications', request.form['image'])
+
+        if not os.path.exists(icon):
+            icon = os.path.join(RUNDIR, 'static', 'images', 'maraschino_logo.png')
 
 
-    if icon[-3:] == "png":
-        icon_type = ICON_PNG
-    elif icon[-3:] == "jpg":
-        icon_type = ICON_JPEG
-    elif icon[-4:] == "jpeg":
-        icon_type = ICON_JPEG
-    elif icon[-3:] == "gif":
-        icon_type = ICON_GIF
+        if icon[-3:] == "png":
+            icon_type = ICON_PNG
+        elif icon[-3:] == "jpg":
+            icon_type = ICON_JPEG
+        elif icon[-4:] == "jpeg":
+            icon_type = ICON_JPEG
+        elif icon[-3:] == "gif":
+            icon_type = ICON_GIF
+        else:
+            icon_type = ICON_NONE
+
+        addr = (hostname, port)
+        sock = socket(AF_INET,SOCK_DGRAM)
+
+        try:
+            logger.log('NOTIFY XBMC :: Sending message to %s' % label, 'INFO')
+            packet = PacketNOTIFICATION(title, message, icon_type, icon)
+            packet.send(sock, addr)
+            return jsonify({ 'status': 'successful'})
+        except:
+            logger.log('NOTIFY XBMC :: Message failed to send', 'ERROR')
+            return jsonify({ 'error': 'Message failed to send'})
     else:
-        icon_type = ICON_NONE
 
-    addr = (hostname, port)
-    sock = socket(AF_INET,SOCK_DGRAM)
-
-    try:
-        logger.log('NOTIFY XBMC :: Sending message to %s' % label, 'INFO')
-        packet = PacketNOTIFICATION(title, message, icon_type, icon)
-        packet.send(sock, addr)
-        return jsonify({ 'status': 'successful'})
-    except:
-        logger.log('NOTIFY XBMC :: Message failed to send', 'ERROR')
-        return jsonify({ 'error': 'Message failed to send'})
+        try:
+            logger.log('NOTIFY XBMC :: Sending message %s' % label, 'INFO')
+            server = PLEXLibrary(server_address())
+            for connectedclient in server.getclients():
+                client = PLEXClient(connectedclient['host'], connectedclient['port'])
+                client.sendmessage(title + "," + message)
+                
+            return jsonify({'status': 'successful'})
+        except Exception, e:
+            logger.log('NOTIFY XBMC :: Message failed to send', 'ERROR')
+            logger.log('NOTIFY XBMC :: Error %s' % e, 'ERROR')
+            return jsonify({'error': 'Message failed to send'})
